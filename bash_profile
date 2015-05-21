6b10e8d7412b15b75e7f6b0213ece961d004fc63
Chad Gilbert <chad.gilbert@forces.gc.ca>
2015-05-21 12:04:58 -0300

    Updated these for my Ubuntu 14.04 machine.

--- bash_profile
+++ bash_profile
@@ -3,15 +3,15 @@
 DOECHO=FALSE
 
 if [ $DOECHO ]; then
-	echo "Sourcing bash_profile."
+    echo "Sourcing bash_profile."
 fi
 
 # Get the aliases, functions, exports.
 for file in bashrc; do
-	if [ -f ~/.$file ]; then
-		. ~/.$file
-		if [ $DOECHO ]; then
-			echo "Sourcing $file."
-		fi
-	fi
+    if [ -f ~/.$file ]; then
+        . ~/.$file
+        if [ $DOECHO ]; then
+            echo "Sourcing $file."
+        fi
+    fi
 done
--- linkdots.sh
+++ linkdots.sh
@@ -2,16 +2,20 @@
 
 # Confirm with user that this is acceptable.
 for file in vimrc aliases bash_profile bashrc exports exports_private gitconfig; do
-	if [ -L ~/.$file ]; then
-		rm ~/.$file
-	elif [ -f ~/.$file ]; then
-		mv ~/.$file ~/.$file.old
-	elif [ -d ~/.$file ]; then
-		mv -rf ~/.$file ~/.$file.old
-	fi
-	cp -v ./$file ~/.$file
+    if [ -L ~/.$file ]; then
+        rm ~/.$file
+    elif [ -f ~/.$file ]; then
+        mv ~/.$file ~/.$file.old
+    elif [ -d ~/.$file ]; then
+        mv -rf ~/.$file ~/.$file.old
+    fi
+    cp -v ./$file ~/.$file
 done
 
-cp -r vim/* ~/.vim/
+if [ -d $HOME/.vim ]; then
+    cp -r vim/* $HOME/.vim/
+else
+    cp -r vim $HOME/.vim
+fi
 
 echo "Your dotfiles are now up-to-date."

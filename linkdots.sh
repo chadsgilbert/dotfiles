c288db53a3eab378f38ed2854b1b03b12ce63d9c
Chad Gilbert <chad.s.gilbert@gmail.com>
2019-01-27 21:14:37 -0400

    Added spellcheck and ale.

--- linkdots.sh
+++ linkdots.sh
@@ -2,7 +2,10 @@
 
 mkdir -p $HOME/bin;
 cp scripts/* $HOME/bin
-echo $(dirname $(realpath "$0"))
+
+
+dfdir=$(dirname $(realpath "$0"))
+
 # Confirm with user that this is acceptable.
 for file in aliases bashrc exports exports_private gitconfig clang-format tmux.conf; do
     if [ -L $HOME/.$file ]; then
@@ -12,18 +15,18 @@ for file in aliases bashrc exports exports_private gitconfig clang-format tmux.c
     elif [ -d $HOME/.$file ]; then
         rm -rfi $HOME/.$file
     fi
-    ln -s $(dirname $(realpath "$0"))/$file $HOME/.$file
+    ln -s $dfdir/$file $HOME/.$file
 done
 
 if [ -d $HOME/.vim ]; then
     rm -rfi $HOME/.vim
 fi
-ln -s $(dirname $(realpath "$0"))/vim $HOME/.vim
+ln -s $dfdir/vim $HOME/.vim
 
 if [ -d $HOME/.gittemplate ]; then
     rm -rfi $HOME/.gittemplate
 else
-	ln -s $(dirname $(realpath "$0"))/gittemplate $HOME/.gittemplate
+	ln -s $dfdir/gittemplate $HOME/.gittemplate
 fi
 
 echo "Your dotfiles are now up-to-date."
